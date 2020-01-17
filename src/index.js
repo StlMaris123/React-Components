82bb2421fdea939dfc917f091a99b197851e6731
StlMaris123 <stellamarisnjage@gmail.com>
2020-01-17 17:16:00 +0200

    display custom children

--- src/index.js
+++ src/index.js
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Faker from 'faker';
 import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="stella" timeRecorded="Today at 2:00 P.M" details="I love you"/>
-            <CommentDetail author="Maris"  timeRecorded="Yesterday at 5:00 A.M" details="I hate you"/>
-            <CommentDetail author="Njage" timeRecorded="Today  at 8:30 A.M" details="I miss you"/>
+            <ApprovalCard >
+              <CommentDetail author="stella" timeRecorded="Today at 2:00 P.M" details="I love you"/>
+            </ApprovalCard>
+            <ApprovalCard >
+              <CommentDetail author="Maris"  timeRecorded="Yesterday at 5:00 A.M" details="I hate you"/>                
+            </ApprovalCard>
+            <ApprovalCard >
+              <CommentDetail author="Njage" timeRecorded="Today  at 8:30 A.M" details="I miss you"/>            
+            </ApprovalCard>
             
         </div>
 
